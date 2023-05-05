c74ee6647f53e1055a074fb85ffc8caf7cc834dc
alireza.rahnama <alirezarm3@gmail.com>
2023-05-05 00:12:28 -0230

    added the public ip for amazon ec2

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api =  axios.create({
-    baseURL:'http://localhost:8080',
+    // baseURL:'http://localhost:8080',
+    baseURL:'https://54.86.96.77:8080', //MY AMAZON EC2 PUBLIC IP ADDRESS
     headers: {"Acess-Control-Allow-Origin": "true"}
 });
 
